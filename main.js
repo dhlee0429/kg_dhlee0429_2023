bc5011b05f9ecdf4a0ecabc4e69e029d6f126b95
dhlee0429 <55640506+dhlee0429@users.noreply.github.com>
2020-03-13 22:55:01 -0400

    Update main.js
    count unique characters in each argument

--- main.js
+++ main.js
@@ -3,3 +3,23 @@ var s2 = process.argv[3];
 
 console.log(s1);
 console.log(s2);
+if (s1.length == s2.length) {
+    var a = [];
+    var b = [];
+    
+    for (var i = 0; i < s1.length; i++) {
+        if (a[s1[i].charCodeAt(0)] == null) {
+            a[s1[i].charCodeAt(0)] = 1;
+        } else {
+            a[s1[i].charCodeAt(0)]++;
+        }
+        if (b[s2[i].charCodeAt(0)] == null) {
+            b[s2[i].charCodeAt(0)] = 1;
+        } else {
+            b[s2[i].charCodeAt(0)]++;
+        }
+    }
+    console.log(a);
+    console.log(b);
+
+}
